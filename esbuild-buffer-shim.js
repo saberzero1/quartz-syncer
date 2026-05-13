6353279e86d681167a70d034044a6792804c3a02
saberzero1 <github@emilebangma.com>
2026-05-13 02:40:54 +0200

    chore: eslint

--- esbuild-buffer-shim.js
+++ esbuild-buffer-shim.js
@@ -32,7 +32,7 @@ if (Platform.isMobileApp) {
 	/* eslint-disable-next-line @typescript-eslint/no-require-imports -- Buffer shim is required for Obsidian mobile */
 	buffer = require("buffer/index.js").Buffer;
 } else {
-	/* eslint-disable-next-line obsidianmd/prefer-active-doc, no-undef -- Buffer shim is required for Obsidian mobile */
+	/* eslint-disable-next-line obsidianmd/prefer-active-doc, obsidianmd/no-global-this, no-undef -- Buffer shim is required for Obsidian mobile */
 	buffer = global.Buffer;
 }
 
